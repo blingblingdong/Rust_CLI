bfbf76eebcf4c55be5e2d2d2027163e0ef5b25d2
blingblingdong <whoareyouman0.4@gmail.com>
2024-08-14 14:25:10 +0800

    ddd

--- headr/src/main.rs
+++ headr/src/main.rs
@@ -1,6 +1,14 @@
+use rand::seq::SliceRandom; // 0.7.2
+
 fn main() {
+
+    let mut vs = vec![0, 1, 2, 3, 4];
+    vs.shuffle(&mut rand::thread_rng());
+    println!("{:?}", vs);
+
    if let Err(e) = headr::get_args().and_then(headr::run) {
      eprintln!("{}", e);
      std::process::exit(1);
    }
+
 }
